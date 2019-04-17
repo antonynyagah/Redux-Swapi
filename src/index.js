f72808a19ad7e3d880e9090bac964012d88f7aa1
Antony Nyagah <antonynyagah@antonys-MacBook-Pro.local>
2019-04-16 21:29:31 -0500

    mvp

--- src/index.js
+++ src/index.js
@@ -3,21 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-import { applyMiddleware, createStore } from 'redux';
-// thunk from redux-thunk
+import { applyMiddleware, createStore } from "redux";
 import thunk from 'redux-thunk';
-// logger from redux-logger
 import logger from 'redux-logger';
-// rootReducer from ./reducers
+import rootReducer from "./reducers";
+
+
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-  applyMiddleware(thunk)
+  rootReducer, applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
